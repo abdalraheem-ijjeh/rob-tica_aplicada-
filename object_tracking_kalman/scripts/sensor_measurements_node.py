151f0af6c3fca27be0b5b54b232e1253893ca696
Abdalraheem Ijjeh <abdalraheem.ijjeh@gmail.com>
2024-02-21 19:41:28 +0100

    updated sensor_measurements_node

--- object_tracking_kalman/scripts/sensor_measurements_node.py
+++ object_tracking_kalman/scripts/sensor_measurements_node.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import PointStamped
 
 import random
 
+filtered_data = None
 
 class Object:
     def __init__(self, X, Y, time=1 / 24):
@@ -21,49 +22,46 @@ class Object:
     def get(self):
         return int(self.x), int(self.y)
 
-
 global obj
 
 obj = Object(250, 250)
 
-global map_env
+map_env = np.zeros((500, 500))
 
 
 def callback(data):
-    global pub, map_env
+    global pub, map_env, filtered_data
     filtered_data = data.point
-    cv2.rectangle(map_env, (int(filtered_data.y) - 10, int(filtered_data.x) - 10),
-                  (int(filtered_data.y + 10), int(filtered_data.x + 10)),
-                  (255, 255, 255), thickness=1)
-
-    print(filtered_data.x, filtered_data.y)
-
+    cv2.circle(map_env, (int(filtered_data.y), int(filtered_data.x)), 10, (255, 255, 255), thickness=1)
+    
 
 def publisher():
     global map_env, obj
-    rospy.init_node('array_publisher', anonymous=True)
+    rospy.init_node('Sensor_node', anonymous=False)
     pub = rospy.Publisher('sensor_data', Float64MultiArray, queue_size=15)
     rospy.Subscriber('filtered_position', PointStamped, callback)
     rate = rospy.Rate(10)  # 1 Hz
     while not rospy.is_shutdown():
         obj.update()
         measurement = obj.get()
+        
+
         if 0 < measurement[0] < 500 and 0 < measurement[1] < 500:
-            map_env = np.zeros((500, 500))
+            
             map_env[measurement] = 255
             cv2.imshow('measurements', map_env)
-            msg_pub = Float64MultiArray()
-            msg_pub.data = measurement
+            rospy.loginfo("Published Array: \n%s", measurement)
+            cv2.waitKey(10)
+            map_env = np.zeros((500, 500))
+        else:
+            map_env = np.zeros((500, 500))
+            obj = Object(250, 250)
+            
+        msg_pub = Float64MultiArray()
+        msg_pub.data = measurement
 
             # Publish the message
-            pub.publish(msg_pub)
-
-            rospy.loginfo("Published Array: \n%s", measurement)
-
-            cv2.waitKey(10)
-        else:
-            obj = Object(250, 250)
-
+        pub.publish(msg_pub)
         rate.sleep()
     cv2.destroyAllWindows()
 
